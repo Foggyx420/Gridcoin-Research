cc0c9967329fc201b82f3fa90e21b58c913ee6fa
Paul Jensen <eastendmumbles@gmail.com>
2017-10-29 18:27:15 -0700

    changes

--- src/compress.h
+++ src/compress.h
@@ -5,7 +5,6 @@ int smaz_compress(const char *in, int inlen, char *out, int outlen);
 int smaz_decompress(const char *in, int inlen, char *out, int outlen);
 
 // Gridcoin
-typedef std::vector<std::pair<std::string, std::string>> CTxMessage;
 
 void CompressTxMessage(const std::string& sMessage, std::string& sReturnCompressed);
 void DecompressTxMessage(const std::string& sMessage, std::string& sReturnDecompressed);
