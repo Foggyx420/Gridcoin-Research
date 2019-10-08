238fcc1ee10774da31903797f9f2d0e0376d3004
Paul Jensen <eastendmumbles@gmail.com>
2019-10-07 20:16:02 -0700

    revert to 1.2.0

--- depends/packages/libzip.mk
+++ depends/packages/libzip.mk
@@ -1,8 +1,8 @@
 package=libzip
-$(package)_version=1.5.2
+$(package)_version=1.2.0
 $(package)_download_path=https://libzip.org/download/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=be694a4abb2ffe5ec02074146757c8b56084dbcebf329123c84b205417435e15
+$(package)_sha256_hash=6cf9840e427db96ebf3936665430bab204c9ebbd0120c326459077ed9c907d9f
 $(package)_dependencies=zlib
 
 define $(package)_config_cmds
